6fe8f48dd9ea91f49eae52044870494e43c41048
Harshad Sonar <61082703+Harshadsonar@users.noreply.github.com>
2023-06-18 15:53:03 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
 () => {
   cy.visit(baseUrl + "/main.html");
-  cy.get(".right").should("be.visible").click();
-  cy.get(".carousel-item.fourth-slide");
+  cy.get(".carousel-item.third-slide").should("have.class", "active");
+}
+() => {
+  cy.visit(baseUrl + "/main.html");
+  cy.get(".carousel-control-prev").should("have.class", "carousel-control");
+  cy.get(".carousel-control-next").should("have.class", "carousel-control");
+  cy.get(".carousel-control-prev .carousel-control-prev-icon").should("have.class", "glyphicon-chevron-left");
+  cy.get(".carousel-control-next .carousel-control-next-icon").should("have.class", "glyphicon-chevron-right");
 }
